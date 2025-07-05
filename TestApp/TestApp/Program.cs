035b86eb28a19cbe3cb27b6cb2971584ce8a0a57
Wolfgang Förster <wolfoerster@gmx.de>
2025-07-05 20:18:23 +0200

    added a TestApp

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SmartLogging;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press [Esc] to quit ...");
+
+            var logger = new SmartLogger();
+            while (true)
+            {
+                var keyInfo = Console.ReadKey();
+                logger.Information(keyInfo);
+
+                if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
