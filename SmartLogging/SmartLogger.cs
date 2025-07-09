a05fd05670713a19242c7354e029abb62b9b08f3
Wolfgang Foerster <wf@labcognition.com>
2025-07-09 14:40:00 +0200

    add comments

--- SmartLogging/SmartLogger.cs
+++ SmartLogging/SmartLogger.cs
@@ -15,88 +15,127 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 //******************************************************************************************
 
-namespace SmartLogging
+namespace SmartLogging;
+
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+public class SmartLogger
 {
-    using System;
-    using System.Diagnostics;
-    using System.Runtime.CompilerServices;
-    using System.Threading;
+    private readonly string className;
 
-    public class SmartLogger
+    /// <summary>
+    /// Creates a new SmartLogger with an optional log context.
+    /// The log context can be a string, a type or the instance of an object.
+    /// It is used to set the 'Class' property of each log entry made by this logger.
+    /// <para/>
+    /// 1. if 'context' is a string, this string will be used as class name
+    /// <para/>
+    /// 2. if 'context' is a type, the full typename will be used as class name
+    /// <para/>
+    /// If 'context' is an instance of an object, the object's type will go into 2.
+    /// <para/>
+    /// If 'context' is null, the type of the method which calls this ctor will go into 2.
+    /// </summary>
+    /// <param name="context">The log context.</param>
+    public SmartLogger(object context = null)
     {
-        private readonly string className;
-
-        public SmartLogger(object context = null)
+        if (context == null)
         {
-            if (context == null)
-            {
-                var stackTrace = new StackTrace();
-                var method = stackTrace.GetFrame(1).GetMethod();
-                context = method.DeclaringType;
-            }
-
-            this.className = GetClassName(context);
+            var stackTrace = new StackTrace();
+            var method = stackTrace.GetFrame(1).GetMethod();
+            context = method.DeclaringType;
         }
 
-        public void Verbose(object msg = null, [CallerMemberName] string methodName = null)
+        this.className = GetClassName(context);
+    }
+
+    private static string GetClassName(object context)
+    {
+        if (context is Type type)
+            return type.FullName;
+
+        if (context is string className)
         {
-            this.Write(msg, LogLevel.Verbose, methodName);
+            if (string.IsNullOrWhiteSpace(className))
+                return "-?-";
+
+            return className;
         }
 
-        public void Debug(object msg = null, [CallerMemberName] string methodName = null)
-        {
-            this.Write(msg, LogLevel.Debug, methodName);
-        }
+        return context.GetType().FullName;
+    }
 
-        public void Information(object msg = null, [CallerMemberName] string methodName = null)
-        {
-            this.Write(msg, LogLevel.Information, methodName);
-        }
+    /// <summary>
+    /// Creates a log entry with level Verbose.
+    /// </summary>
+    public void Verbose(object msg = null, [CallerMemberName] string methodName = null)
+    {
+        this.Write(msg, LogLevel.Verbose, methodName);
+    }
 
-        public void Warning(object msg = null, [CallerMemberName] string methodName = null)
-        {
-            this.Write(msg, LogLevel.Warning, methodName);
-        }
+    /// <summary>
+    /// Creates a log entry with level Debug.
+    /// </summary>
+    public void Debug(object msg = null, [CallerMemberName] string methodName = null)
+    {
+        this.Write(msg, LogLevel.Debug, methodName);
+    }
 
-        public void Error(object msg = null, [CallerMemberName] string methodName = null)
-        {
-            this.Write(msg, LogLevel.Error, methodName);
-        }
+    /// <summary>
+    /// Creates a log entry with level Information.
+    /// </summary>
+    public void Information(object msg = null, [CallerMemberName] string methodName = null)
+    {
+        this.Write(msg, LogLevel.Information, methodName);
+    }
 
-        public void Fatal(object msg = null, [CallerMemberName] string methodName = null)
-        {
-            this.Write(msg, LogLevel.Fatal, methodName);
-        }
+    /// <summary>
+    /// Creates a log entry with level Warning.
+    /// </summary>
+    public void Warning(object msg = null, [CallerMemberName] string methodName = null)
+    {
+        this.Write(msg, LogLevel.Warning, methodName);
+    }
 
-        public void None(object msg = null, [CallerMemberName] string methodName = null)
-        {
-            this.Write(msg, LogLevel.None, methodName);
-        }
+    /// <summary>
+    /// Creates a log entry with level Error.
+    /// </summary>
+    public void Error(object msg = null, [CallerMemberName] string methodName = null)
+    {
+        this.Write(msg, LogLevel.Error, methodName);
+    }
 
-        public void Exception(Exception exception, [CallerMemberName] string methodName = null)
-        {
-            this.Fatal(new { ExceptionMessage = exception.GetMessage(), exception.StackTrace }, methodName);
-        }
+    /// <summary>
+    /// Creates a log entry with level Fatal.
+    /// </summary>
+    public void Fatal(object msg = null, [CallerMemberName] string methodName = null)
+    {
+        this.Write(msg, LogLevel.Fatal, methodName);
+    }
 
-        public void Write(object msg, LogLevel level, [CallerMemberName] string methodName = null)
-        {
-            LogWriter.Write(msg, level, className, methodName);
-        }
+    /// <summary>
+    /// Creates a log entry with level None.
+    /// </summary>
+    public void None(object msg = null, [CallerMemberName] string methodName = null)
+    {
+        this.Write(msg, LogLevel.None, methodName);
+    }
 
-        private static string GetClassName(object context)
-        {
-            if (context is Type type)
-                return type.FullName;
+    /// <summary>
+    /// Creates a log entry with level Error that contains exception details.
+    /// </summary>
+    public void Exception(Exception exception, [CallerMemberName] string methodName = null)
+    {
+        this.Error(new { ExceptionMessage = exception.GetMessage(), exception.StackTrace }, methodName);
+    }
 
-            if (context is string className)
-            {
-                if (string.IsNullOrWhiteSpace(className))
-                    return "-?-";
-
-                return className;
-            }
-
-            return context.GetType().FullName;
-        }
+    /// <summary>
+    /// Creates a log entry with the specified level.
+    /// </summary>
+    public void Write(object msg, LogLevel level, [CallerMemberName] string methodName = null)
+    {
+        LogWriter.Write(msg, level, className, methodName);
     }
 }
